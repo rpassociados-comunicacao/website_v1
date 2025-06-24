d67ba29a96b0681f45818f9af3ebd8fe92cdab87
gmartinsfaria <g.martinsfaria@ua.pt>
2025-06-24 15:53:24 +0100

    Alterações landpage 4 - Tamanho destaques

--- js/team.js
+++ js/team.js
@@ -184,7 +184,30 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
-
+document.addEventListener("DOMContentLoaded", function () {
+    const targetElement = document.getElementById("SectionLeft");
+    const mediaQuery = window.matchMedia("(min-width: 768px)");
+  
+    function updateClasses(e) {
+      if (!targetElement) return;
+  
+      if (e.matches) {
+        // Largura >= 768px
+        targetElement.classList.add("container-left");
+        targetElement.classList.remove("container");
+        targetElement.classList.remove("px-5");
+      } else {
+        // Largura < 768px
+        targetElement.classList.remove("container-left");
+      }
+    }
+  
+    // Executa à carga
+    updateClasses(mediaQuery);
+  
+    // E ao mudar de tamanho
+    mediaQuery.addEventListener("change", updateClasses);
+});
 
 
 
