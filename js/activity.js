a1e21ec6d86ecca98fc82ca853a588440e377624
gmartinsfaria <g.martinsfaria@ua.pt>
2025-07-16 17:45:56 +0100

    Adicionei ano dinâmico no copyright das páginas de detalhes

--- js/activity.js
+++ js/activity.js
@@ -318,3 +318,10 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 /* ****************************************** SCROLL ANIM FIM ***************************************** */
+
+document.addEventListener("DOMContentLoaded", function () {
+    const d = new Date();
+    let year = d.getFullYear();
+    console.log("Estamos no ano " + year);
+    document.getElementById("currentYear").innerHTML = year;
+});
--- js/manifesto.js
+++ js/manifesto.js
@@ -459,3 +459,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
 /* ****************************************** SCROLL ANIM FIM ***************************************** */
 
+document.addEventListener("DOMContentLoaded", function () {
+    const d = new Date();
+    let year = d.getFullYear();
+    console.log("Estamos no ano " + year);
+    document.getElementById("currentYear").innerHTML = year;
+});
