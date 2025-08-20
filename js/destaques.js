bcecea27dda9dca2a965b3406990a519e4d5165f
gmartinsfaria <g.martinsfaria@ua.pt>
2025-08-20 18:11:09 +0100

    Alterações backend 2

--- js/destaques.js
+++ js/destaques.js
@@ -445,7 +445,7 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
 
-  fetch("https://www.rpaadvogados.com/assets/json-files/articles.json") //https://www.rpaadvogados.com/assets/json-files/articles.json
+  fetch("https://sheetdb.io/api/v1/74klqjvml6bo1") //https://www.rpaadvogados.com/assets/json-files/articles.json
     .then(response => response.json())
     .then(data => {
       allArticles = data;
