a28da3012f2e5bd3c496580141d9ee36a26b6b2e
gmartinsfaria <g.martinsfaria@ua.pt>
2026-01-03 23:09:53 +0000

    Correção erro direcionamento para páginas de detalhes dos artigos através do index

--- js/index.js
+++ js/index.js
@@ -1341,7 +1341,7 @@ function generateArticles() {
         const div = document.createElement("div");
         div.className = "news-item hidden-until-ready";
         div.innerHTML = `
-          <a href="/destaques/detalhe/?id=${id}" target="_blank" class="news-detail-link">
+          <a href="/opinioes/detalhe/?id=${id}" target="_blank" class="news-detail-link">
             <div class="news-img-wrapper">
               <img src="${artigo.thumbnail}" alt="${artigo.titulo || 'Thumbnail notícia'}">
             </div>
